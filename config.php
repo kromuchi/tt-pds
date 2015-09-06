fd20cac59d7038e2abbaa02372a815dffb51573e
kromuchi <valentin.falkenhahn+git@gmail.com>
2015-09-06 17:17:39 +0200

    new default settings

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
 # Insert master key here to call admin interface using ?admin=getallaccesscodes&mkey=somerandombutcomplexmasterkey
 $TTPDS_masterkey 		= "somerandombutcomplexmasterkey";
 
-# Truncates the $_GET["acc"] variable to prevent random attacs
-$TTPDS_keys_maxlen 	= 50;
+# Truncates the $_GET["acc"] variable to prevent random attacks (if not exists, the length of all access keys in data/data.php +10% is taken as limit.
+# $TTPDS_keys_maxlen 	= 50;
 
 # Datafolder without trailing '/'
 $TTPDS_datafolder		= "data";
@@ -14,7 +14,7 @@ $TTPDS_datafolder		= "data";
 # Extra copyright domain (if blank, root domain is used)
 $TTPDS_extra_copyright = "vfalkenhahn.de";
 
-# Available languages (uncomment 
+# Available languages (uncomment to disable)
 $TTPDS_langs = array(
         'en', //default
         'de',
